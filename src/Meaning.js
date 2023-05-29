479f3485bf84826215107221de62acbc93a06685
tania <tanakorchynska@gmail.com>
2023-05-30 01:14:44 +0300

    Created a Synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -10,6 +11,8 @@ export default function Meaning(props) {
         {props.meaning.definition}
         <br />
         <em>{props.meaning.example}</em>
+        <br />
+        <Synonyms synonyms={props.meaning.synonyms} />
       </p>
     </div>
   );
