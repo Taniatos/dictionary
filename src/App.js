092e5437574f0b4a98ce9100d094ca46bab63046
tania <tanakorchynska@gmail.com>
2023-06-01 00:47:24 +0300

    Linked the github code; fixed styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-
-import './App.css';
-import Dictionary from "./Dictionary"
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <main>
-        <Dictionary defaultKeyword="heart"/>
+        <Dictionary defaultKeyword="heart" />
       </main>
-      <footer className="text-center">The app is coded by Tetiana Korchynska and is open-sourced</footer>
+      <footer className="text-center">
+        Open-sourced{" "}
+        <a
+          href="https://github.com/Taniatos/dictionary.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          code
+        </a>{" "}
+        by Tetiana Korchynska
+      </footer>
     </div>
   );
 }
-
-
